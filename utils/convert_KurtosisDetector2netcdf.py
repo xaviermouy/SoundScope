e8074b8dd1ce5e34583023570124788f0b752459
Xavier Mouy <xavier.mouy@gmail.com>
2024-11-25 12:21:32 -0500

    kurt

--- utils/convert_KurtosisDetector2netcdf.py
+++ utils/convert_KurtosisDetector2netcdf.py
@@ -12,10 +12,10 @@ import pandas as pd
 import uuid
 
 ## User inputs
-detec_files_dir =r'C:\Users\xavier.mouy\Documents\Projects\2024_NERACOOS_Soundscape_GOM\Analysis\data\KurtosisDetector\NEFSC_SBNMS_201908_SB03' # location of the detection files
-audio_files_dir =r'F:\NEFSC_SBNMS_201908_SB03\1677778970_48kHz' # Location of the audio files associated with the detections
-nc_file_out_dir = r'C:\Users\xavier.mouy\Documents\Projects\2024_NERACOOS_Soundscape_GOM\Analysis\data\KurtosisDetector\NEFSC_SBNMS_201908_SB03' # where the nc files will be created
-nc_file_out_name = 'NEFSC_SBNMS_201908_SB03_Kurtosis.nc'
+detec_files_dir =r'C:\Users\xavier.mouy\Documents\Projects\2024_NERACOOS_Soundscape_GOM\Analysis\data\KurtosisDetector\NEFSC_SBNMS_201910_SB03' # location of the detection files
+audio_files_dir =r'F:\NEFSC_SBNMS_201910_SB03\1678008346_48kHz' # Location of the audio files associated with the detections
+nc_file_out_dir = r'C:\Users\xavier.mouy\Documents\Projects\2024_NERACOOS_Soundscape_GOM\Analysis\data\KurtosisDetector\NEFSC_SBNMS_201910_SB03' # where the nc files will be created
+nc_file_out_name = 'NEFSC_SBNMS_201910_SB03_Kurtosis.nc'
 audio_file_extension = '.wav' # extention of the audio files in audio_files_dir
 
 ## script
